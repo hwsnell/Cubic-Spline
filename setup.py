0b41218990d9b35c4ff1f48a57bca96c71d1a092
harrison-snell <harrison.snell23@gmail.com>
2023-05-19 08:58:31 -0400

    working on setup

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name = 'cubic_spline',
-    packages = ['cubic_spline'],
+    name = 'Cubic-Spline',
+    packages = ['Cubic-Spline'],
     version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
     description = 'Cubic Spline Interpolation for Value Function Iteration',
     author = 'Harrison Snell',
-    author_email = '',
-    url = '',
-    download_url = '',
-    keywords = [],
+    author_email = 'harrison.snell23@gmail.com',
+    url = 'https://github.com/hwsnell/Cubic-Spline',
     classifiers = ['License :: OSI Approved :: MIT License', 'Operating System :: OS Independent', 'Programming Language :: Python :: 3']
 )
